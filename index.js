6a6b98aa2a7d5f33f3ba9670ed54d480e94b6d38
shoronshaha <smaranshaha@gmail.com>
2022-05-15 19:57:15 +0600

    join to database collection useing react query method

--- index.js
+++ index.js
@@ -30,17 +30,33 @@ async function run() {
 
         })
 
-
+        // warning
+        // This is not the proper way to query
+        // After learning more about mongodb. use aggregate lookup, pipeline, match, group
         app.get('/available', async (req, res) => {
-            const date = req.query.date || 'May 15, 2022';
+            const date = req.query.date;
 
             // step 1: get All services
             const services = await servicesCollection.find().toArray();
+            // step 2: get the booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // step 3: for each service, find bookings for that service
+            services.forEach(service => {
+                // step 4: find bookings for that service. output : [{}, {}, {}]
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                // step 5: select slots for the service Bookings: ["", "", "" ,""]
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                // step 6: select those slots that are not in bookedSlots
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                // step 7: set available to slots to make it easier
+                service.slots = available;
+            })
             res.send(services);
         })
         /*
-       Api naming convention
-       
+        Api naming convention
         * app.get('/booking') // get all booking in this collection. or get more than 
         one or by filter
         *app.get('/booking/:id') // get a specific booking
