a64fdaf145eb9ae246d932d826c80e6eb37906be
shoronshaha <smaranshaha@gmail.com>
2022-05-16 19:54:17 +0600

    added jwt token for user validation and secure the data

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -40,7 +41,9 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+            res.send({ result, token });
         })
 
         // warning
